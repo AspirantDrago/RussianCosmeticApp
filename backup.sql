3a2a8c8b53bc5a7d2391be72882a35de481ce49c
Aleksandr <sanekclubstr@mail.com>
2023-12-15 01:14:47 +0300

    Change encoding to UTF-8

--- backup.sql
+++ backup.sql
@@ -2,10 +2,10 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Дек 14 2023 г., 10:02
--- Версия сервера: 10.3.22-MariaDB
--- Версия PHP: 7.1.33
+-- РҐРѕСЃС‚: 127.0.0.1:3306
+-- Р’СЂРµРјСЏ СЃРѕР·РґР°РЅРёСЏ: Р”РµРє 14 2023 Рі., 10:02
+-- Р’РµСЂСЃРёСЏ СЃРµСЂРІРµСЂР°: 10.3.22-MariaDB
+-- Р’РµСЂСЃРёСЏ PHP: 7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,25 +18,25 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `russian_cosmetic`
+-- Р‘Р°Р·Р° РґР°РЅРЅС‹С…: `russian_cosmetic`
 --
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `clients`
+-- РЎС‚СЂСѓРєС‚СѓСЂР° С‚Р°Р±Р»РёС†С‹ `clients`
 --
 
 CREATE TABLE `clients` (
-  `client_id` int(11) NOT NULL COMMENT 'ID клиента',
+  `client_id` int(11) NOT NULL COMMENT 'ID РєР»РёРµРЅС‚Р°',
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'E-mail',
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Хэш пароля',
-  `is_ur` tinyint(1) NOT NULL COMMENT 'Является ли клиент юридическим лицом',
-  `removed` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Удалён ли клиент'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Таблица клиентов';
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РҐСЌС€ РїР°СЂРѕР»СЏ',
+  `is_ur` tinyint(1) NOT NULL COMMENT 'РЇРІР»СЏРµС‚СЃСЏ Р»Рё РєР»РёРµРЅС‚ СЋСЂРёРґРёС‡РµСЃРєРёРј Р»РёС†РѕРј',
+  `removed` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'РЈРґР°Р»С‘РЅ Р»Рё РєР»РёРµРЅС‚'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='РўР°Р±Р»РёС†Р° РєР»РёРµРЅС‚РѕРІ';
 
 --
--- Дамп данных таблицы `clients`
+-- Р”Р°РјРї РґР°РЅРЅС‹С… С‚Р°Р±Р»РёС†С‹ `clients`
 --
 
 INSERT INTO `clients` (`client_id`, `email`, `password`, `is_ur`, `removed`) VALUES
@@ -48,152 +48,152 @@ INSERT INTO `clients` (`client_id`, `email`, `password`, `is_ur`, `removed`) VAL
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `clients_phys`
+-- РЎС‚СЂСѓРєС‚СѓСЂР° С‚Р°Р±Р»РёС†С‹ `clients_phys`
 --
 
 CREATE TABLE `clients_phys` (
-  `client_id` int(11) NOT NULL COMMENT 'ID клиента',
-  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Имя',
-  `surname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Фамилия',
-  `patronymic` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Отчество',
-  `birthday` date NOT NULL COMMENT 'Дата рождения',
-  `passport` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Серия и номер паспорта',
-  `phone` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Номер телефона'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Таблица клиентов - физических лиц';
+  `client_id` int(11) NOT NULL COMMENT 'ID РєР»РёРµРЅС‚Р°',
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РРјСЏ',
+  `surname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Р¤Р°РјРёР»РёСЏ',
+  `patronymic` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РћС‚С‡РµСЃС‚РІРѕ',
+  `birthday` date NOT NULL COMMENT 'Р”Р°С‚Р° СЂРѕР¶РґРµРЅРёСЏ',
+  `passport` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РЎРµСЂРёСЏ Рё РЅРѕРјРµСЂ РїР°СЃРїРѕСЂС‚Р°',
+  `phone` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РќРѕРјРµСЂ С‚РµР»РµС„РѕРЅР°'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='РўР°Р±Р»РёС†Р° РєР»РёРµРЅС‚РѕРІ - С„РёР·РёС‡РµСЃРєРёС… Р»РёС†';
 
 --
--- Дамп данных таблицы `clients_phys`
+-- Р”Р°РјРї РґР°РЅРЅС‹С… С‚Р°Р±Р»РёС†С‹ `clients_phys`
 --
 
 INSERT INTO `clients_phys` (`client_id`, `name`, `surname`, `patronymic`, `birthday`, `passport`, `phone`) VALUES
-(10, 'Иван', 'Иванов', 'Иванович', '2023-12-13', '0111111111', '70222222222'),
-(11, 'Пётр', 'Петров', 'Петрович', '2023-12-13', '0333333333', '74444444444'),
-(12, 'Семён', 'Семёнов', 'Семёнович', '2023-12-13', '5555555555', '76666666666'),
+(10, 'РРІР°РЅ', 'РРІР°РЅРѕРІ', 'РРІР°РЅРѕРІРёС‡', '2023-12-13', '0111111111', '70222222222'),
+(11, 'РџС‘С‚СЂ', 'РџРµС‚СЂРѕРІ', 'РџРµС‚СЂРѕРІРёС‡', '2023-12-13', '0333333333', '74444444444'),
+(12, 'РЎРµРјС‘РЅ', 'РЎРµРјС‘РЅРѕРІ', 'РЎРµРјС‘РЅРѕРІРёС‡', '2023-12-13', '5555555555', '76666666666'),
 (14, '1', '1', '1', '2023-12-13', '0111111111', '70111111111');
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `clients_ur`
+-- РЎС‚СЂСѓРєС‚СѓСЂР° С‚Р°Р±Р»РёС†С‹ `clients_ur`
 --
 
 CREATE TABLE `clients_ur` (
-  `client_id` int(11) NOT NULL COMMENT 'ID клиента',
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Название компании',
-  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Адрес',
-  `INN` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'ИНН',
-  `rs` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Расчётный счёт',
-  `BIK` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'БИК',
-  `name_head` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Имя руководителя',
-  `surname_head` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Фамилия руководителя',
-  `patronymic_head` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Отчество руководителя',
-  `name_contact` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Имя контактного лица',
-  `surname_contact` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Фамилия контактного лица',
-  `patronymic_contact` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Отчество контактного лица',
-  `phone_contact` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Телефон контактного лица'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Таблица клиентов - юридических лиц';
+  `client_id` int(11) NOT NULL COMMENT 'ID РєР»РёРµРЅС‚Р°',
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РќР°Р·РІР°РЅРёРµ РєРѕРјРїР°РЅРёРё',
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РђРґСЂРµСЃ',
+  `INN` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РРќРќ',
+  `rs` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Р Р°СЃС‡С‘С‚РЅС‹Р№ СЃС‡С‘С‚',
+  `BIK` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Р‘РРљ',
+  `name_head` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РРјСЏ СЂСѓРєРѕРІРѕРґРёС‚РµР»СЏ',
+  `surname_head` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Р¤Р°РјРёР»РёСЏ СЂСѓРєРѕРІРѕРґРёС‚РµР»СЏ',
+  `patronymic_head` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РћС‚С‡РµСЃС‚РІРѕ СЂСѓРєРѕРІРѕРґРёС‚РµР»СЏ',
+  `name_contact` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РРјСЏ РєРѕРЅС‚Р°РєС‚РЅРѕРіРѕ Р»РёС†Р°',
+  `surname_contact` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Р¤Р°РјРёР»РёСЏ РєРѕРЅС‚Р°РєС‚РЅРѕРіРѕ Р»РёС†Р°',
+  `patronymic_contact` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РћС‚С‡РµСЃС‚РІРѕ РєРѕРЅС‚Р°РєС‚РЅРѕРіРѕ Р»РёС†Р°',
+  `phone_contact` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РўРµР»РµС„РѕРЅ РєРѕРЅС‚Р°РєС‚РЅРѕРіРѕ Р»РёС†Р°'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='РўР°Р±Р»РёС†Р° РєР»РёРµРЅС‚РѕРІ - СЋСЂРёРґРёС‡РµСЃРєРёС… Р»РёС†';
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `orders`
+-- РЎС‚СЂСѓРєС‚СѓСЂР° С‚Р°Р±Р»РёС†С‹ `orders`
 --
 
 CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL COMMENT 'ID заказа',
-  `data_create` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Дата и время создания',
-  `status` int(11) NOT NULL DEFAULT 1 COMMENT 'ID статуса заказа',
-  `duration` float NOT NULL DEFAULT 0 COMMENT 'Длительность, ч',
-  `client` int(11) NOT NULL COMMENT 'ID клиента',
-  `removed` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'Удалено'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Таблица заказов';
+  `order_id` int(11) NOT NULL COMMENT 'ID Р·Р°РєР°Р·Р°',
+  `data_create` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Р”Р°С‚Р° Рё РІСЂРµРјСЏ СЃРѕР·РґР°РЅРёСЏ',
+  `status` int(11) NOT NULL DEFAULT 1 COMMENT 'ID СЃС‚Р°С‚СѓСЃР° Р·Р°РєР°Р·Р°',
+  `duration` float NOT NULL DEFAULT 0 COMMENT 'Р”Р»РёС‚РµР»СЊРЅРѕСЃС‚СЊ, С‡',
+  `client` int(11) NOT NULL COMMENT 'ID РєР»РёРµРЅС‚Р°',
+  `removed` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'РЈРґР°Р»РµРЅРѕ'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='РўР°Р±Р»РёС†Р° Р·Р°РєР°Р·РѕРІ';
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `orders_servieces`
+-- РЎС‚СЂСѓРєС‚СѓСЂР° С‚Р°Р±Р»РёС†С‹ `orders_servieces`
 --
 
 CREATE TABLE `orders_servieces` (
-  `order_id` int(11) NOT NULL COMMENT 'ID заказа',
-  `service_id` int(11) NOT NULL COMMENT 'ID услуги',
-  `status` int(11) NOT NULL COMMENT 'ID статуса',
-  `datetime_comlplete` timestamp NULL DEFAULT NULL COMMENT 'Дата и время завершения',
-  `employee` int(11) DEFAULT NULL COMMENT 'ID сотрудника'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Связь между заказом и услугами "многие-ко-многим"';
+  `order_id` int(11) NOT NULL COMMENT 'ID Р·Р°РєР°Р·Р°',
+  `service_id` int(11) NOT NULL COMMENT 'ID СѓСЃР»СѓРіРё',
+  `status` int(11) NOT NULL COMMENT 'ID СЃС‚Р°С‚СѓСЃР°',
+  `datetime_comlplete` timestamp NULL DEFAULT NULL COMMENT 'Р”Р°С‚Р° Рё РІСЂРµРјСЏ Р·Р°РІРµСЂС€РµРЅРёСЏ',
+  `employee` int(11) DEFAULT NULL COMMENT 'ID СЃРѕС‚СЂСѓРґРЅРёРєР°'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='РЎРІСЏР·СЊ РјРµР¶РґСѓ Р·Р°РєР°Р·РѕРј Рё СѓСЃР»СѓРіР°РјРё "РјРЅРѕРіРёРµ-РєРѕ-РјРЅРѕРіРёРј"';
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `services`
+-- РЎС‚СЂСѓРєС‚СѓСЂР° С‚Р°Р±Р»РёС†С‹ `services`
 --
 
 CREATE TABLE `services` (
-  `service_id` int(11) NOT NULL COMMENT 'ID услуги',
-  `service_title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Наименование услуги',
-  `price` decimal(11,2) NOT NULL COMMENT 'Цена, руб',
-  `duration` float NOT NULL COMMENT 'Длительность, ч',
-  `std` double NOT NULL DEFAULT 0 COMMENT 'Стандартное отклонение'
+  `service_id` int(11) NOT NULL COMMENT 'ID СѓСЃР»СѓРіРё',
+  `service_title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РќР°РёРјРµРЅРѕРІР°РЅРёРµ СѓСЃР»СѓРіРё',
+  `price` decimal(11,2) NOT NULL COMMENT 'Р¦РµРЅР°, СЂСѓР±',
+  `duration` float NOT NULL COMMENT 'Р”Р»РёС‚РµР»СЊРЅРѕСЃС‚СЊ, С‡',
+  `std` double NOT NULL DEFAULT 0 COMMENT 'РЎС‚Р°РЅРґР°СЂС‚РЅРѕРµ РѕС‚РєР»РѕРЅРµРЅРёРµ'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Дамп данных таблицы `services`
+-- Р”Р°РјРї РґР°РЅРЅС‹С… С‚Р°Р±Р»РёС†С‹ `services`
 --
 
 INSERT INTO `services` (`service_id`, `service_title`, `price`, `duration`, `std`) VALUES
-(3, 'Водородный показатель pH', '500.00', 0.25, 0.5),
-(4, 'Кислотное число', '1500.00', 1.5, 0.01),
-(5, 'Содержание фенола', '3000.00', 1.25, 0.01),
-(6, 'Стойкость пены', '3500.00', 6, 0.01),
-(7, 'Содержание жирных кислот', '2500.00', 2, 0.01);
+(3, 'Р’РѕРґРѕСЂРѕРґРЅС‹Р№ РїРѕРєР°Р·Р°С‚РµР»СЊ pH', '500.00', 0.25, 0.5),
+(4, 'РљРёСЃР»РѕС‚РЅРѕРµ С‡РёСЃР»Рѕ', '1500.00', 1.5, 0.01),
+(5, 'РЎРѕРґРµСЂР¶Р°РЅРёРµ С„РµРЅРѕР»Р°', '3000.00', 1.25, 0.01),
+(6, 'РЎС‚РѕР№РєРѕСЃС‚СЊ РїРµРЅС‹', '3500.00', 6, 0.01),
+(7, 'РЎРѕРґРµСЂР¶Р°РЅРёРµ Р¶РёСЂРЅС‹С… РєРёСЃР»РѕС‚', '2500.00', 2, 0.01);
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `statuses`
+-- РЎС‚СЂСѓРєС‚СѓСЂР° С‚Р°Р±Р»РёС†С‹ `statuses`
 --
 
 CREATE TABLE `statuses` (
-  `status_id` int(11) NOT NULL COMMENT 'ID статуса',
-  `status_title` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Статус'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Таблица статусов';
+  `status_id` int(11) NOT NULL COMMENT 'ID СЃС‚Р°С‚СѓСЃР°',
+  `status_title` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'РЎС‚Р°С‚СѓСЃ'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='РўР°Р±Р»РёС†Р° СЃС‚Р°С‚СѓСЃРѕРІ';
 
 --
--- Дамп данных таблицы `statuses`
+-- Р”Р°РјРї РґР°РЅРЅС‹С… С‚Р°Р±Р»РёС†С‹ `statuses`
 --
 
 INSERT INTO `statuses` (`status_id`, `status_title`) VALUES
-(2, 'В процессе'),
-(4, 'Выполнен'),
-(3, 'Готов к выдаче'),
-(5, 'Отменён'),
-(1, 'Создан');
+(2, 'Р’ РїСЂРѕС†РµСЃСЃРµ'),
+(4, 'Р’С‹РїРѕР»РЅРµРЅ'),
+(3, 'Р“РѕС‚РѕРІ Рє РІС‹РґР°С‡Рµ'),
+(5, 'РћС‚РјРµРЅС‘РЅ'),
+(1, 'РЎРѕР·РґР°РЅ');
 
 --
--- Индексы сохранённых таблиц
+-- РРЅРґРµРєСЃС‹ СЃРѕС…СЂР°РЅС‘РЅРЅС‹С… С‚Р°Р±Р»РёС†
 --
 
 --
--- Индексы таблицы `clients`
+-- РРЅРґРµРєСЃС‹ С‚Р°Р±Р»РёС†С‹ `clients`
 --
 ALTER TABLE `clients`
   ADD PRIMARY KEY (`client_id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Индексы таблицы `clients_phys`
+-- РРЅРґРµРєСЃС‹ С‚Р°Р±Р»РёС†С‹ `clients_phys`
 --
 ALTER TABLE `clients_phys`
   ADD PRIMARY KEY (`client_id`);
 
 --
--- Индексы таблицы `clients_ur`
+-- РРЅРґРµРєСЃС‹ С‚Р°Р±Р»РёС†С‹ `clients_ur`
 --
 ALTER TABLE `clients_ur`
   ADD PRIMARY KEY (`client_id`);
 
 --
--- Индексы таблицы `orders`
+-- РРЅРґРµРєСЃС‹ С‚Р°Р±Р»РёС†С‹ `orders`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`order_id`),
@@ -201,71 +201,71 @@ ALTER TABLE `orders`
   ADD KEY `client` (`client`);
 
 --
--- Индексы таблицы `orders_servieces`
+-- РРЅРґРµРєСЃС‹ С‚Р°Р±Р»РёС†С‹ `orders_servieces`
 --
 ALTER TABLE `orders_servieces`
   ADD PRIMARY KEY (`order_id`,`service_id`);
 
 --
--- Индексы таблицы `services`
+-- РРЅРґРµРєСЃС‹ С‚Р°Р±Р»РёС†С‹ `services`
 --
 ALTER TABLE `services`
   ADD PRIMARY KEY (`service_id`),
   ADD UNIQUE KEY `service_title` (`service_title`);
 
 --
--- Индексы таблицы `statuses`
+-- РРЅРґРµРєСЃС‹ С‚Р°Р±Р»РёС†С‹ `statuses`
 --
 ALTER TABLE `statuses`
   ADD PRIMARY KEY (`status_id`),
   ADD UNIQUE KEY `status_title` (`status_title`);
 
 --
--- AUTO_INCREMENT для сохранённых таблиц
+-- AUTO_INCREMENT РґР»СЏ СЃРѕС…СЂР°РЅС‘РЅРЅС‹С… С‚Р°Р±Р»РёС†
 --
 
 --
--- AUTO_INCREMENT для таблицы `clients`
+-- AUTO_INCREMENT РґР»СЏ С‚Р°Р±Р»РёС†С‹ `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID клиента', AUTO_INCREMENT=15;
+  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID РєР»РёРµРЅС‚Р°', AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT для таблицы `orders`
+-- AUTO_INCREMENT РґР»СЏ С‚Р°Р±Р»РёС†С‹ `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID заказа', AUTO_INCREMENT=2;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID Р·Р°РєР°Р·Р°', AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT для таблицы `services`
+-- AUTO_INCREMENT РґР»СЏ С‚Р°Р±Р»РёС†С‹ `services`
 --
 ALTER TABLE `services`
-  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID услуги', AUTO_INCREMENT=8;
+  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID СѓСЃР»СѓРіРё', AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT для таблицы `statuses`
+-- AUTO_INCREMENT РґР»СЏ С‚Р°Р±Р»РёС†С‹ `statuses`
 --
 ALTER TABLE `statuses`
-  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID статуса', AUTO_INCREMENT=6;
+  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID СЃС‚Р°С‚СѓСЃР°', AUTO_INCREMENT=6;
 
 --
--- Ограничения внешнего ключа сохраненных таблиц
+-- РћРіСЂР°РЅРёС‡РµРЅРёСЏ РІРЅРµС€РЅРµРіРѕ РєР»СЋС‡Р° СЃРѕС…СЂР°РЅРµРЅРЅС‹С… С‚Р°Р±Р»РёС†
 --
 
 --
--- Ограничения внешнего ключа таблицы `clients_phys`
+-- РћРіСЂР°РЅРёС‡РµРЅРёСЏ РІРЅРµС€РЅРµРіРѕ РєР»СЋС‡Р° С‚Р°Р±Р»РёС†С‹ `clients_phys`
 --
 ALTER TABLE `clients_phys`
   ADD CONSTRAINT `clients_phys_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`);
 
 --
--- Ограничения внешнего ключа таблицы `clients_ur`
+-- РћРіСЂР°РЅРёС‡РµРЅРёСЏ РІРЅРµС€РЅРµРіРѕ РєР»СЋС‡Р° С‚Р°Р±Р»РёС†С‹ `clients_ur`
 --
 ALTER TABLE `clients_ur`
   ADD CONSTRAINT `clients_ur_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`);
 
 --
--- Ограничения внешнего ключа таблицы `orders`
+-- РћРіСЂР°РЅРёС‡РµРЅРёСЏ РІРЅРµС€РЅРµРіРѕ РєР»СЋС‡Р° С‚Р°Р±Р»РёС†С‹ `orders`
 --
 ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`status`) REFERENCES `statuses` (`status_id`),
